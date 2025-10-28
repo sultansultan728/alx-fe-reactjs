9a372bd44c075a53c2ccd9ffeb01c5658ff41dc0
sultansultan728 <sultansultan7659999@gmail.com>
2025-10-28 09:28:35 -0700

    Modify JSX in a Pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import WelcomeMessage from './components/WelcomeMessage';
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  );
+}
+
+export default App;
+
 function App() {
   const [count, setCount] = useState(0)
 
