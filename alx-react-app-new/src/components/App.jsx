d393ecb883abe3a09b16261d3e4779648063f692
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-09 09:52:03 -0800

    task1

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -3,14 +3,14 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import Counter from './components/Counter'; // ✅ Import Counter
+import Counter from './components/Counter'; 
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
-      <Counter /> {/* ✅ Add Counter component here */}
+      <Counter /> 
       <Footer />
     </div>
   );
