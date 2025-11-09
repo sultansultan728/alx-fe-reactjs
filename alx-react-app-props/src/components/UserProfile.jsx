ae2db7454804abf2cb2710d8822ffd22b578c704
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-09 10:21:09 -0800

    task2

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import UserInfo from './UserInfo';
+import UserContext from './UserContext'; 
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext); 
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Welcome, {userData.name}!</p>
+      <UserInfo />
+    </div>
+  );
 }
 
 export default UserProfile;
