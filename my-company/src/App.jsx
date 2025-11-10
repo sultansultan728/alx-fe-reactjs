33aacf3d4a9ae07af5ec5408149b3cd492a99fa0
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-10 04:17:34 -0800

    task4

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,32 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-
-
 function App() {
   const [count, setCount] = useState(0)
 
