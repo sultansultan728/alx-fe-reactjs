dc09811db6d33f5f771e6750e27dd1d2b6d9f36d
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-13 06:11:52 -0800

    1. Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,5 +5,19 @@ export const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
+
+updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
