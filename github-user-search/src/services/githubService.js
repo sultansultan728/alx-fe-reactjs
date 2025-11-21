8c7bf0b7b0004b43c23b89dfbf8107398738ff86
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-21 06:55:31 -0800

    2. Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const url = `https://api.github.com/users/${username}`;
+export const searchUsers = async ({ username, location, minRepos, page }) => {
+  let query = "";
+
+  if (username) query += `${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>${minRepos} `;
+
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(
+    query
+  )}&page=${page}&per_page=10`;
 
   const response = await axios.get(url);
-  return response.data;
+
+  return {
+    users: response.data.items,
+    hasMorePages: response.data.total_count > page * 10
+  };
 };
 
