c5caf3fa3fae539e355064c1c047e931bb2566ef
sultansultan728 <sultansultan7659999@gmail.com>
2025-11-30 06:52:18 -0800

    1. Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
